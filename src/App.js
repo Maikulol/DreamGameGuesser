bf0d1706123b09d95d360b41bf872b5e73f57fb5
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-09 23:08:24 -0500

    Co-authored-by: Omar A. <HyperVS@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='Buttons'>
+        <Button text="Engine"/>
+        <Button text="Games"/>
+        <Button text="About"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
