1fc527034731382affd72936304befb39185f678
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-10 10:45:51 -0500

    Made new page

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+function About(){
+    return (
+        <div className="AboutContainer">
+            <div className="AboutTextContainer">
+                <div className="BodyParagraphTitle">
+                    <h1>About the project</h1>
+                </div>
+                <div className="BodyParagraph">
+                    <h4>Blank test text</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Title from './components/Title';
+import About from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
       <Title/>
-      <App />
+      {/* <App /> */}
+      <About/>
     </>
   </React.StrictMode>
 );
