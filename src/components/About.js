ae719bba6ffa7203a602e6c948c58d47364cc6e7
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-12 01:13:55 -0500

    Added image

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,11 @@ function About(){
                     <h1>About the project</h1>
                 </div>
                 <div className="BodyParagraph">
-                    <h4>Blank test text</h4>
+                    <h4>My name is Mike, and I am the creator of this site. 
+                        This project has been a goal of mine for over 2 years now, ever since its first 
+                        iteration that I designed in grade 10. Since then, my coding knowledge has advanced 
+                        exponentially, and my ideas have grown with it. I hope this creation offers
+                        some entertainment for you.</h4>
                 </div>
             </div>
         </div>
