1c66dbe74b2f7fc20b591f72fdd238b52b76d937
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-13 17:15:39 -0500

    Changed font and styling for scroll bar.
    Co-authored-by: Omar A. <HyperVS@users.noreply.github.com>

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,19 @@ function About(){
         <div className="About">
             <div className="TextContainer">
                 <div className="AboutTitle">
-                    <h1>About the project</h1>
+                    <h1>About The Project</h1>
                 </div>
                 <div className="Paragraph">
                     <h4>My name is Mike, and I am the creator of this site. 
-                        This project has been a goal of mine for over 2 years now, ever since its first 
-                        iteration that I designed in grade 10. Since then, my coding knowledge has advanced 
-                        exponentially, and my ideas have grown with it. I hope this creation offers
-                        some entertainment for you. Remember to enjoy it!</h4>
+                        This project has been a goal of mine for over 2 years now, 
+                        ever since its first 
+                        iteration that I designed in grade 10. 
+                        Since then, my coding knowledge has advanced 
+                        exponentially, and my ideas have grown with it. 
+                        I hope this creation offers
+                        some entertainment for you. 
+                        Remember to enjoy it! 
+                    </h4>
                 </div>
             </div>
         </div>
