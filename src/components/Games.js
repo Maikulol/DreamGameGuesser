129187f8d0c54d3879769e3e06a4c9b7b00a79b6
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-17 00:25:25 -0500

    Created functioning search bar

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,136 +1,44 @@
 import '../styles/Games.css'
 import Game from './Game'
 import SearchBar from './SearchBar'
+import { useState, useEffect } from "react";
 
 function Games(){
+    
+    const [list, setList] = useState([])
+    const [userInput, updateValue] = useState("")
+    const [fullList, setFullList] = useState([{
+        title: "Valorant",
+        url: "www.someurl.com"
+    }, {
+        title: "CSGO",
+        url: "www.csgo.com"
+    }, {
+        title: "Rocket League",
+        url: "www.rl.com"
+    }])
+    
+    
+    useEffect(() => {
+        if(userInput !== ""){
+            setList(fullList.filter(game => game.title.toLowerCase().includes(userInput.toLowerCase())))
+        } else {
+            setList(fullList)
+        }
+    }, [userInput, fullList])
+
     return(
         <div className="Games">
             <div className='SearchBarContainer'>
-                <SearchBar/>
+                <SearchBar value={userInput} onChange={(e) => updateValue(e.target.value)}/>
             </div>
             <div className='GamesListContainer'>
                 <ul className="GamesList">
-                    {GamesList.map((game) => <Game title={game.title} imageUrl={game.url}/>)}
+                    {list.map((game) => <Game title={game.title} imageUrl={game.url}/>)}
                 </ul>
             </div>
         </div>
     )
 }
 
-const GamesList = [{
-    title: "Valorant",
-    url: "www.someurl.com"
-} , {
-    title: "CSGO",
-    url: "www.csgo.com"
-}, {
-    title: "Rocket League",
-    url: "www.rl.com"
-}, {
-    title: "CSGO",
-    url: "www.csgo.com"
-}, {
-    title: "Rocket League",
-    url: "www.rl.com"
-}, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "Valorant",
-        url: "www.someurl.com"
-    } , {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }, {
-        title: "CSGO",
-        url: "www.csgo.com"
-    }, {
-        title: "Rocket League",
-        url: "www.rl.com"
-    }];
-
-    export default Games;
+export default Games;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,9 @@
 import '../styles/SearchBar.css'
 
-function SearchBar(){
+
+function SearchBar(props){
     return (
-        <input className='SearchBar' type='text' placeholder='Search Games Here' spellCheck="false"/>
+        <input className='SearchBar' type='text' placeholder='Search Games Here' spellCheck="false" value={props.value} onChange={props.onChange}/>
     )
 }
 
