b6eb50020b28baa68b5c6d3810244e5e09fbb064
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-20 10:42:24 -0500

    Added robot to Engine

--- src/components/Engine.js
+++ src/components/Engine.js
@@ -7,31 +7,9 @@ function Engine(){
                 <div className='EngineTitle'>
                     <h1>THE GAME GUESSER</h1>
                 </div>
-                <div className='EngineInput'>
-                    <div className='EngineInputTitle'>
-                        <h2>What are your video game preferences?</h2>
-                    </div>
-                    <div className='InputButtons'>
-                        Adventure?
-                    </div>
-                    <div className='InputButtons'>
-                        Shooter?
-                    </div>
-                    <div className='InputButtons'>
-                        Fighting?
-                    </div>
-                    <div className='InputButtons'>
-                        Sport?
-                    </div>
-                    <div className='InputButtons'>
-                        Simulator?
-                    </div>
-                </div>
-                <div className='EngineOutput'>
-                    <div className='EngineOutputTitle'>
-                        <h2>Based on your preferences, 
-                        we recommend that you check out this game/list of games.</h2>
-                    </div>
+                <div className='EngineProcessing'>
+                    <div className='EngineRobot'/>
+                    <div className='EngineText'/>
                 </div>
             </div>
         </div>
@@ -39,3 +17,30 @@ function Engine(){
 }
 
 export default Engine;
+
+/* <div className='EngineInput'>
+<div className='EngineInputTitle'>
+    <h2>What are your video game preferences?</h2>
+</div>
+<div className='InputButtons'>
+    Adventure?
+</div>
+<div className='InputButtons'>
+    Shooter?
+</div>
+<div className='InputButtons'>
+    Fighting?
+</div>
+<div className='InputButtons'>
+    Sport?
+</div>
+<div className='InputButtons'>
+    Simulator?
+</div>
+</div>
+<div className='EngineOutput'>
+<div className='EngineOutputTitle'>
+    <h2>Based on your preferences, 
+    we recommend that you check out this game/list of games.</h2>
+</div>
+</div> */
