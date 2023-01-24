de7ad526f6d56a7cedade2d44b7d5b5e421acaf7
Maikulol <91214380+Maikulol@users.noreply.github.com>
2023-01-24 16:00:34 -0500

    Finished user input and engine page

--- Frontend/src/components/Engine.js
+++ Frontend/src/components/Engine.js
@@ -14,10 +14,11 @@ function Engine(){
         [2, "What type of game do you prefer?"],
         [3, "Do you like shooter games?"],
         [4, "Do you like adventure games?"],
-        [5, "Are you interested in sports games or racing games?"],
+        [5, "Do you enjoy simulators?"],
         [6, "Are you a role player?"],
-        [7, "Do you enjoy simulators?"],
-        [8, "Do you like Hack and Slash games?"]
+        [7, "Are you interested in sports games?"],
+        [8, "Are you interested in racing games?"],
+        [9, "Do you like Hack and Slash games?"]
     ])
     
     // Set of answers for first button
@@ -26,10 +27,11 @@ function Engine(){
         [2, "SINGLEPLAYER"],
         [3, "YES"],
         [4, "YES"],
-        [5, "SPORT"],
+        [5, "YES"],
         [6, "YES"],
         [7, "YES"],
-        [8, "YES"]
+        [8, "YES"],
+        [9, "YES"]
     ])
 
     // Set of answers for second button
@@ -38,10 +40,11 @@ function Engine(){
         [2, "MULTIPLAYER"],
         [3, "NO"],
         [4, "NO"],
-        [5, "RACING"],
+        [5, "NO"],
         [6, "NO"],
         [7, "NO"],
-        [8, "NO"]
+        [8, "NO"],
+        [9, "NO"]
     ])
     
     // useState is used to update the onscreen text such as the question being asked and the text on the buttons
@@ -50,6 +53,7 @@ function Engine(){
     const [question, setQuestion] = useState(questionSet.get(questionCount));
     const [leftText, setLeftText] = useState(answerSet1.get(questionCount));
     const [rightText, setRightText] = useState(answerSet2.get(questionCount));
+    const [end, setEnd] = useState(false);
 
     // Changes the question and buttons on screen according to the questionCount
     const updateScreen = () => {
@@ -64,26 +68,53 @@ function Engine(){
         setQuestionCount(questionCount + 1);
     }
 
-    // Updates the recored user input
-    const updateUserInput = (text) => {
-        setUserInput(map => new Map(map.set(questionCount, text)))
-    }
-
     // Function used exclusively for skipping certain questions that may be irrelevant
     const jumpToQuestion = (number) => {
         setQuestionCount(number)
     }
-
+    
+    // Updates the recored user input
+    const updateUserInput = (text) => {
+        setUserInput(map => new Map(map.set(questionCount, text)))
+    }
+    
+    // Logic flow for input questions/answers
+    // Uses if statements to avoid contradicting questions
     useEffect(() => {
+
+        // If you like shooter games, or you like adventure games, skip simulators
+        if((userInput.get(3) == "YES" || userInput.get(4) == "YES") && questionCount == 5) jumpToQuestion(6);
+
+        // If you like simulators, skip role playing games
+        if(userInput.get(5) == "YES" && questionCount == 6) jumpToQuestion(7);
+        
+        // If you like adventure games, shooter games, or role playing games, skip simulator games
+        if(userInput.get(5) == undefined && questionCount == 7) jumpToQuestion(8);
+
+        // If you like shooter games, skip hack and slash games
+        if(userInput.get(3) == "YES" && questionCount == 7) setEnd(true);
+
+        // If you like RPG games, skip sports and racing games
+        if(userInput.get(6) == "YES" && questionCount == 7) jumpToQuestion(9);
+        
+        // If you like sport games, racing games, or simulator games, skip hack and slash games and provide output
+        if(userInput.get(7) != undefined && questionCount == 9) setEnd(true);
+        
+        // If you answer the last question in the question list, provide output
+        if(userInput.get(9) != undefined && questionCount == 10) setEnd(true);
+        
         updateScreen();
     }, [questionCount])
-
+    
+    console.log(questionCount)
+    // console.log(userInput)
     const NewFilteredList = [
         {
             name: "COCK",
             url: "https://twitter.com/home?lang=en",
             cover: "https://images.igdb.com/igdb/image/upload/t_cover_big/co4ymo.png"
-        },{
+        },
+        {
             name: "COCK",
             url: "https://twitter.com/home?lang=en",
             cover: "https://images.igdb.com/igdb/image/upload/t_cover_big/co4ymo.png"
@@ -110,21 +141,23 @@ function Engine(){
                     <div className='EngineRobot'>
                         <img src={robot} className="EngineRobotImage"/>
                     </div>
-                    {/* <div className='EngineTextBox'>
-                        <div className='EngineText tri-right left-in'>
-                            {question}
+                    {end != true ?
+                        <div className='EngineTextBox'>
+                            <div className='EngineText tri-right left-in'>
+                                {question}
+                            </div>
+                            <div className='EngineButtons'>
+                                <EngineButton text={leftText} onClick={() => goToNextQuestion(leftText)}/>
+                                <EngineButton text={rightText} onClick={() => goToNextQuestion(rightText)}/>
+                            </div>
+                        </div> : 
+                        <div className='EngineOutput'>
+                            <h1 className='EngineOutputTitle'>Games tailored for you!</h1>
+                            <ul className="EngineOutputGames">
+                                {NewFilteredList.map((game, index) => <Game name={game.name} imageUrl={game.cover} url={game.url} width="200px"/>)}
+                            </ul>
                         </div>
-                        <div className='EngineButtons'>
-                            <EngineButton text={leftText} onClick={() => goToNextQuestion(leftText)}/>
-                            <EngineButton text={rightText} onClick={() => goToNextQuestion(rightText)}/>
-                        </div>
-                    </div> */}
-                    <div className='EngineOutput'>
-                        <h1 className='EngineOutputTitle'>Games tailored for you</h1>
-                        <ul className="EngineOutputGames">
-                            {NewFilteredList.map((game, index) => <Game name={game.name} imageUrl={game.cover} url={game.url} width="200px"/>)}
-                        </ul>
-                    </div>
+                    }
                 </div>
             </div>
         </div>
